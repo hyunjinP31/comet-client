d5d7eb8c394a32ec5529288ba2939a988a7d9cec
hyunjinP <wldwlddl0531@gmail.com>
2022-07-25 17:26:59 +0900

    header layout complete

---
+++ src/components/include/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {FiSearch} from 'react-icons/fi'
+
+const Header = () => {
+    return (
+        <>
+            <div className='header inner'>
+                <div className='headerTop'>
+                    <h1 className='headerLogo'>TEMPUS</h1>
+                    <ul>
+                        <li>로그인</li>
+                        <li>회원가입</li>
+                    </ul>
+                </div>
+                <div className='headerBottom'>
+                    <div className='headerLeft'>
+                        <div className='toggle'>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <ul>
+                            <li>인기</li>
+                            <li>신규</li>
+                            <li>마감임박</li>
+                            <li>공개예정</li>
+                        </ul>
+                    </div>
+                    <div className='searchBox'>
+                        <input type='text' name='search' />
+                        <FiSearch className='searchBtn'/>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
