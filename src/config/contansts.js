4aea04874e90963047a47ad48684aeeb0a1ca6a6
hyunjinP <wldwlddl0531@gmail.com>
2022-08-29 12:13:39 +0900

    서버 다시 변경

--- src/config/contansts.js
+++ src/config/contansts.js
@@ -1,6 +1,6 @@
-// //헤로쿠 서버
-// export const API_URL = "https://comet-server.herokuapp.com"
+//헤로쿠 서버
+export const API_URL = "https://comet-server.herokuapp.com"
 //로컬 서버
-export const API_URL = "http://localhost:8080"
+// export const API_URL = "http://localhost:8080"
 //이미지 저장 aws s3
 export const IMG_URL = "https://comet-server-support.s3.ap-northeast-2.amazonaws.com"
